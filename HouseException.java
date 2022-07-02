a341271daed4fa95d054f6d4088837f79351c4d4
Abhiragk17 <106880986+Abhiragk17@users.noreply.github.com>
2022-07-02 21:44:46 +0530

    Add files via upload

---
+++ HouseException.java
@@ -0,0 +1,9 @@
+class HouseException extends Exception {
+
+    HouseException(String s) {
+        super(s);
+    }
+    // public String toString(){
+    //     return "House type not found Exception";
+    // }
+}

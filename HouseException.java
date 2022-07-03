b430a5bd622ea391857dc84f331f9bb000081c6f
Abhiragk17 <106880986+Abhiragk17@users.noreply.github.com>
2022-07-03 12:43:34 +0530

    Update HouseException.java

--- HouseException.java
+++ HouseException.java
@@ -1,9 +1,6 @@
 class HouseException extends Exception {
-
-    HouseException(String s) {
-        super(s);
+    
+    public String toString(){
+        return "House type not found Exception";
     }
-    // public String toString(){
-    //     return "House type not found Exception";
-    // }
 }

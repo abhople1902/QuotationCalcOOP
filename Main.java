b7a2c42b058ecc40fb278ab150f49e118b5cdc95
abhople1902 <77747115+abhople1902@users.noreply.github.com>
2022-07-18 01:05:49 +0530

    Commit#JustBeforeFinal

--- Main.java
+++ Main.java
@@ -1,15 +1,26 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         File filecheck;
         File f = new File("tier2.txt");
-        File f1 = new File("Tier3.txt");
-        File f2 = new File("database_tier1.txt");
+        File f1 = new File("tier3.txt");
+        File f2 = new File("tier1.txt");
+        File f3 = new File("HouseType.txt");
         BufferedReader in = new BufferedReader(new FileReader(f));
         BufferedReader in1 = new BufferedReader(new FileReader(f1));
         BufferedReader in2 = new BufferedReader(new FileReader(f2));
+        BufferedReader in3 = new BufferedReader(new FileReader(f3));
+
+        String[] arr2;
+        String special;
+        while ((special=in3.readLine()) != null){
+            arr2 = special.split("    ");
+        }
+
         String str;
         String cityName;
         String type;
@@ -19,7 +30,7 @@ public class Main {
         boolean tier1 = false,tier2 = false,tier3 = false;
         double plotsize;
         int choice = 1;
-        while(choice){
+        while(choice!=0){
             System.out.println("Enter the city name");
             cityName = sc.next();
             System.out.println("Enter the plot size");
@@ -32,11 +43,13 @@ public class Main {
             String strfinal1[];
             String strfinal2[];
             String strfinal3[];
+
+
             while((str = in.readLine())!=null){
                 str1 = str.split("    ");
                 if(cityName.equals(str1[0])) {
                     check = true;
-                    Strfinal1 = str.split("    ");
+                    strfinal1 = str.split("    ");
                     tier2 = true;
                     filecheck = f;
                 }
@@ -46,7 +59,7 @@ public class Main {
                 str2 = str.split("    ");
                 if(cityName.equals(str2[0])) {
                     check = true;
-                    Strfinal2 = str.split("    ");
+                    strfinal2 = str.split("    ");
                     tier3 = true;
                     filecheck = f1;
                 }
@@ -59,7 +72,7 @@ public class Main {
                 str3 = str.split("    ");
                 if(cityName.equals(str3[0])) {
                     check = true;
-                    Strfinal3 = str.split("    ");
+                    strfinal3 = str.split("    ");
                     tier1 = true;
                     filecheck = f2;
 
@@ -68,16 +81,18 @@ public class Main {
                     check = false;
                 }
             }
+
+
         }
         if(check == false){
-            throw new CityNotFoundException(cityName);
+            throw new CityNotFoundException();
         }
 
         if(type.equal("Duplex") || type.equals("Flat") || type.equals("Bunglow")){
             typecheck = true;
         }
         else{
-            throw new HouseException(type);
+            throw new HouseException();
         }
 
         if(tier1){
@@ -90,15 +105,17 @@ public class Main {
 
         if(typecheck){
             if(type.equals("Duplex")){
-                Duplex d = new Duplex(cityName,tier,plotsize,filecheck);
+                Duplex d = new Duplex(cityName,tier,plotsize,filecheck,str2[0]);
                 d.getInfo();
                 d.calculatecosts();
+                long estimateCost = d.totalEstimate();
+                System.out.println("The quotation for Duplex is: " + estimateCost);
             } else if (type.equals("Flat")) {
-                Flat fl = new Flat(tier,cityName,plotsize);
+                Flat fl = new Flat(tier,cityName,plotsize,str2[2]);
             } else if (type.equals("Bunglow")) {
-                bunglow b = new bunglow(type,cityName,plotsize)
+                bunglow b = new bunglow(type,cityName,plotsize,str2[1]);
             } else if (type.equals("FarmHouse")) {
-                FarmHouse fm = new FarmHouse(1,plotsize,cityName);
+                FarmHouse fm = new FarmHouse(1,plotsize,cityName,str2[3]);
             }
         }
 
